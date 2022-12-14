c8567dbc5ac7026f3b713776d4053a730038089e
Jaume Barcelo <jaume.barcelo@gmail.com>
2022-12-14 11:59:17 +0000

    feature:activates button

--- models/library_book.py
+++ models/library_book.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from odoo.exceptions import ValidationError
 
 class Book(models.Model):
     _name = "library.book"
@@ -12,5 +13,21 @@ class Book(models.Model):
     publisher_id = fields.Many2one("res.partner", string="Editorial")
     author_ids = fields.Many2many("res.partner", string="Autores")
 
-    def button_check_isbn():
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits)==13:
+            ponderations = [1,3]*6
+            terms=[a*b for a, b in zip(digits[:12],
+                ponderations)]
+            remain = sum(terms)%10
+            check = 10 - remain if remain !=0 else 0
+            return digits[-1] == check
+
+    def button_check_isbn(self):
+        for book in self:
+            if not book.isbn:
+                raise ValidationError("Por favor, proporciona un ISBN para %s" %book.name)
+            if book.isbn and not book._check_isbn():
+                raise ValidationError("El ISBN de %s no es válido" %book.isbn)
         return True
